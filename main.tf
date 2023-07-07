943fb026415b5dfb065aff0ce07137abfd3096ee
Peer <hillspeer@gmail.com>
2023-07-07 15:05:17 +0530

    draft

--- main.tf
+++ main.tf
@@ -1,125 +1,77 @@
+terraform {
+  required_version = ">= 0.12"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+
 ### PROVIDER
-provider "google" {
-  project = "advancedterraform" #replace this with your project-id
-  region  = "us-central1"
-  zone    = "us-central1-a"
-}
-
-### NETWORK
-data "google_compute_network" "default" {
-  name                    = "default"
-}
-
-## SUBNET
-resource "google_compute_subnetwork" "subnet-1" {
-  name                     = "subnet1"
-  ip_cidr_range            = "10.127.0.0/20"
-  network                  = data.google_compute_network.default.self_link
-  region                   = "us-central1"
-  private_ip_google_access = true
-}
-
-resource "google_compute_firewall" "default" {
-  name    = "test-firewall"
-  network = data.google_compute_network.default.self_link
-
-  allow {
-    protocol = "icmp"
-  }
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80", "8080", "1000-2000", "22"]
-  }
-
-  source_tags = ["web"]
-}
-
-### COMPUTE
-## NGINX PROXY
-resource "google_compute_instance" "nginx_instance" {
-  name         = "nginx-proxy"
-  machine_type = "f1-micro"
-  tags = ["web"]
-  
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
+variable "aws_region" {
+  type = map
+  default = {
+      dev="us-east-1" 
+      prod = "ap-south-1"
     }
-  }
-
-  network_interface {
-    network = data.google_compute_network.default.self_link
-    subnetwork = google_compute_subnetwork.subnet-1.self_link
-    access_config {
-      
-    }
-  }
 }
 
-## WEB1
-resource "google_compute_instance" "web1" {
-  name         = "web1"
-  machine_type = "f1-micro"
-  
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    # A default network is created for all GCP projects
-    network = data.google_compute_network.default.self_link
-    subnetwork = google_compute_subnetwork.subnet-1.self_link
-  }
-}
-## WEB2
-resource "google_compute_instance" "web2" {
-  name         = "web2"
-  machine_type = "f1-micro"
-  
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    network = data.google_compute_network.default.self_link
-    subnetwork = google_compute_subnetwork.subnet-1.self_link
-  }
-}
-## WEB3
-resource "google_compute_instance" "web3" {
-  name         = "web3"
-  machine_type = "f1-micro"
-  
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    network = data.google_compute_network.default.self_link
-    subnetwork = google_compute_subnetwork.subnet-1.self_link
-  }  
+provider "aws" {
+  region = var.aws_region[terraform.workspace]
+  access_key = "AKIA2QWNIU65MQA327VJ"
+  secret_key = "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"
 }
 
-## DB
-resource "google_compute_instance" "mysqldb" {
-  name         = "mysqldb"
-  machine_type = "f1-micro"
-  
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
+data "archive_file" "myzip" {
+  type = "zip"
+  source_file = "main.py"
+  output_path = "main.zip"
+}
 
-  network_interface {
-    network = data.google_compute_network.default.self_link
-    subnetwork = google_compute_subnetwork.subnet-1.self_link
-  }  
+resource "aws_lambda_function" "mypython_lambda" {
+  filename = "main.zip"
+  function_name = "${mypython_lambda_test_}terraform.workspace"
+  role = aws_iam_role.mypython_lambda_role.arn
+  #role = AWSLambdaRole.arn
+  handler = "main.lambda_handler"
+  runtime = "python3.8"
+  source_code_hash = "data.archive_file.myzip.output_base64sha256"
+}
+
+resource "aws_iam_role" "mypython_lambda_role" {
+  name = "mypython_role"
+  assume_role_policy = <<-EOF
+  {
+    "Version" : "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "lambda.amazonaws.com"
+        },
+        "Effect":"Allow",
+        "Sid":""
+      }
+    ]
+  }
+  EOF
+}
+
+resource "aws_sqs_queue" "main_queue" {
+  name = "my-main-queue"
+  delay_seconds = 30
+  max_message_size = 262144
+}
+
+resource "aws_sqs_queue" "dlq_queue" {
+  name = "my-dlq-queue"
+  delay_seconds = 30
+  max_message_size = 262144
+}
+
+resource "aws_lambda_event_source_mapping" "sqs-lambda-trigger" {
+  event_source_arn = aws_sqs_queue.main_queue.arn
+  function_name = aws_lambda_function.mypython_lambda.arn
 }
